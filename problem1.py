9796d6680ba83d06df5ce5a5ea5bc6c931945841
JackCpper <fxtdldcf1993@sina.com>
2016-02-25 01:31:27 +0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,9 +1,6 @@
+#code by myself
 sum1=0
 for i in range(1000):
     if (i%3==0)|(i%5==0):
         sum1=sum1+i
 print(sum1)
-
-x,y = [x for x in range(0, 1000, 3)], [y for y in range(0, 1000, 5)]
-xy = list(set(x + y))
-print(sum(xy))
